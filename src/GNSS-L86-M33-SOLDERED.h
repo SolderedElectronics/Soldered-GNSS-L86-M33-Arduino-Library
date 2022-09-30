deb6c01f6b908ab2d6561eb81e9b43072532c7ff
Clang Robot <robot@example.com>
2022-09-30 08:05:22 +0000

    Committing clang-format changes

--- src/GNSS-L86-M33-SOLDERED.h
+++ src/GNSS-L86-M33-SOLDERED.h
@@ -13,8 +13,8 @@
 #define __GNSS_L86_M33_SOLDERED_H__
 
 #include "Arduino.h"
-#include "libs/TinyGPSPlus/src/TinyGPSPlus.h"
 #include "libs/Generic-easyC/easyC.hpp"
+#include "libs/TinyGPSPlus/src/TinyGPSPlus.h"
 
 #if defined(ARDUINO_ESP32_DEV)
 #include "libs/SoftwareSerial/SoftwareSerial.h"
